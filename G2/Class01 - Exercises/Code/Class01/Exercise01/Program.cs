5cf7f1642ec98b42765e7816b33fc626b9cf0d7a
tstojanovska <stojanovskatanja03@gmail.com>
2021-04-19 21:04:33 +0200

    Class01

---
+++ G2/Class01 - Exercises/Code/Class01/Exercise01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Enter a name");
+                string name = Console.ReadLine();
+                names.Add(name);
+
+                Console.WriteLine("Enter x if you like to stop");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "x")
+                    break;
+            }
+
+            Console.WriteLine("Enter text");
+            string text = Console.ReadLine();
+            Dictionary<string, int> result = CountAppearances(names, text);
+            Console.ReadLine();
+        }
+
+        private static Dictionary<string, int> CountAppearances(List<string> names, string text)
+        {
+            Dictionary<string, int> results = new Dictionary<string, int>();
+            string[] textArr = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string name in names)
+            {
+                if (!results.ContainsKey(name))
+                {
+                    results.Add(name, textArr.Count(x => x.Trim().ToLower() == name.Trim().ToLower()));
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ G2/Class01 - Exercises/Code/QuizApp/QuizApp/QuizApp.Domain/Classes/Question.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace QuizApp.Domain.Classes
+{
+    public class Question
+    {
+        public string Description { get; set; }
+        public List<string> Answers { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+}
