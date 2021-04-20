b1e0d244d89a82d63ff7697544f676cb24326fcb
Damjan Stojanovski <damjan.stojanovski@haselt.com>
2021-04-20 20:57:25 +0200

    code added

---
+++ G6/Class01/Code/SEDC.CSharpAdv.Class01/SEDC.CSharpAdv.Class01.ConsoleApp3/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace SEDC.CSharpAdv.Class01.ConsoleApp3
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public UserChoice PlayerChoice { get; set; }
+    }
+}
