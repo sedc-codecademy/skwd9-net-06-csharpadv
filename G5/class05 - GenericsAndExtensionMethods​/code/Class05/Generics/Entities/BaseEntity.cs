ae9022543eaa93100c40f8cc09418cc9b8ee61a6
Viktor Jakovlev <vicebad@hotmail.com>
2021-05-04 21:20:20 +0200

    class 05 init

---
+++ G5/class05 - GenericsAndExtensionMethods​/code/Class05/Generics/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public abstract string GetInfo();
+    }
+}
---
+++ G5/class05 - GenericsAndExtensionMethods​/code/Class05/Generics/Program.cs
@@ -0,0 +1,46 @@
+﻿using Generics.Helpers;
+using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericMethods();
+
+            Console.ReadLine();
+        }
+
+        public static void GenericMethods() 
+        {
+            List<string> strings = new List<string>() { "str1", "str2", "str3" };
+            List<int> ints = new List<int>() { 5, 22, -18 };
+            List<bool> bools = new List<bool> { true, false, true };
+
+            //non generic helpers
+            NotGenericListHelper.GoThroughtStrings(strings);
+            NotGenericListHelper.GetInfoForStrings(strings);
+            Console.WriteLine("========");
+
+            NotGenericListHelper.GoThroughInts(ints);
+            NotGenericListHelper.GetInfoForInts(ints);
+            Console.WriteLine("========");
+            Console.WriteLine("========");
+
+            //generic helpers
+            GenericListHelper.GoThrough<string>(strings);
+            GenericListHelper.GetInfoFor<string>(strings);
+            Console.WriteLine("========");
+
+            GenericListHelper.GoThrough<int>(ints);
+            GenericListHelper.GetInfoFor<int>(ints);
+            Console.WriteLine("========");
+
+            GenericListHelper.GoThrough<bool>(bools);
+            GenericListHelper.GetInfoFor<bool>(bools);
+            Console.WriteLine("========");
+        }
+    }
+}
