52c1d68f3b474787ce1907a9d32bee328febdc40
Viktor Jakovlev <vicebad@hotmail.com>
2021-05-04 19:19:43 +0200

    class06 init

---
+++ G5/class04 - StaticClassesAndPolymorphism/OrderingApp/OrderingApp/OrderingApp/Helpers/TextHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderingApp.Helpers
+{
+    public static class TextHelper
+    {
+        public static string CapitalFirstLetter(string input) 
+        {
+            if (input.Length == 0) 
+            {
+                return "Empty String";
+            }
+
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+
+        public static int ValidateNumberInput(string input) 
+        {
+            bool isMenuChoiceValid = int.TryParse(input, out int choice);
+
+            if (!isMenuChoiceValid) 
+            {
+                Console.WriteLine("Wrong input...");
+                return -1;
+            }
+
+            return choice;
+        }
+    }
+}
---
+++ G5/class04 - StaticClassesAndPolymorphism/OrderingApp/OrderingApp/OrderingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
