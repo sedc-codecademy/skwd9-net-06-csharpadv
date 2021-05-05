100151f2a2c91802adb2466a411ac792f560c9bc
kristina-s <spasevska.kristina87@gmail.com>
2021-05-05 21:37:19 +0200

    G4 class 05 added

---
+++ G4/Class05/SEDC.TryBeingFit/SEDC.TryBeingFit.Domain/Core/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using SEDC.TryBeingFit.Domain.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.TryBeingFit.Domain.Core.Entities
+{
+    public abstract class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+        public abstract string Print();
+    }
+}
---
+++ G4/Class05/SEDC.TryBeingFit/SEDC.TryBeingFit.Domain/Core/Interfaces/ITrainerUser.cs
@@ -0,0 +1,12 @@
+﻿using SEDC.TryBeingFit.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.TryBeingFit.Domain.Core.Interfaces
+{
+    public interface ITrainerUser
+    {
+        bool ChangeSchedule(LiveTraining liveTraining, int days);
+    }
+}
