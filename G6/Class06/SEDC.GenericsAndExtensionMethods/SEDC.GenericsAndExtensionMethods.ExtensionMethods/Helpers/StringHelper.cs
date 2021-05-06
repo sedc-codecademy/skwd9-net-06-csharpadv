1dff7bf8a8012b2ca87a9a4e2022f687a7be6375
Martin <panovski.martin93@gmail.com>
2021-05-06 21:19:55 +0200

    Class 06 demo code added

---
+++ G6/Class06/SEDC.GenericsAndExtensionMethods/SEDC.GenericsAndExtensionMethods.ExtensionMethods/Helpers/StringHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEDC.GenericsAndExtensionMethods.ExtensionMethods.Helpers
+{
+    public static class StringHelper
+    {
+        public static string Shorten(this string str, int numberOfWords)
+        {
+            if (numberOfWords <= 0)
+                throw new ArgumentException("numberOfWords should be greater than or equal to 0");
+
+            if (str.Length == 0)
+                return string.Empty;
+
+            string[] words = str.Split(' ');
+
+            if (words.Length < numberOfWords)
+                return str;
+
+            List<string> substring = words.Take(numberOfWords).ToList();
+
+            string result = string.Join(" ", substring);
+            return result + ".... ----> Read more";
+        }
+    }
+}
