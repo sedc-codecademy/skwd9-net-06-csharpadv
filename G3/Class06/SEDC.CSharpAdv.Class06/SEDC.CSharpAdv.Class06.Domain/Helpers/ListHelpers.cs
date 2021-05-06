1b5403de32b3c5e026588080a3aa565707c69371
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-05-06 19:11:05 +0200

    Class06 part-1

---
+++ G3/Class06/SEDC.CSharpAdv.Class06/SEDC.CSharpAdv.Class06.Domain/Helpers/ListHelpers.cs
@@ -0,0 +1,31 @@
+﻿using SEDC.CSharpAdv.Class06.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace SEDC.CSharpAdv.Class06.Domain.Helpers
+{
+    public static class ListHelper
+	{
+		public static void PrintSimple<T>(this List<T> list)
+		{
+			Console.WriteLine("Printing...");
+			Console.WriteLine("------------------------------");
+			foreach (T item in list)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("------------------------------");
+		}
+
+		public static void PrintEntities<T>(this List<T> list) where T : BaseEntity
+		{
+			Console.WriteLine($"Printing {list[0].GetType().Name}s...");
+			Console.WriteLine("------------------------------");
+			foreach (T item in list)
+			{
+				Console.WriteLine(item.Info());
+			}
+			Console.WriteLine("------------------------------");
+		}
+	}
+}
