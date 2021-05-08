d7cf9c5a81e106e7060798fc713c94c45ad4faf4
tstojanovska <stojanovskatanja03@gmail.com>
2021-05-08 12:41:12 +0200

    Class 06

---
+++ G2/Class06 - Anonymous methods and Advanced LINQ/Code/AdvancedLINQ/AdvancedLINQ/Helpers/ListHelper.cs
@@ -0,0 +1,29 @@
+﻿using AdvancedLINQ.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace AdvancedLINQ.Helpers
+{
+    public static class ListHelper
+    {
+        public static void Print<T>(this List<T> list)
+        {
+            Console.WriteLine("================");
+            foreach(T item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("================");
+        }
+
+        public static void PrintEntities<T> (this List<T> list) where T : BaseEntity
+        {
+            Console.WriteLine("================");
+            foreach (T item in list)
+            {
+                Console.WriteLine(item.GetInfo());
+            }
+            Console.WriteLine("================");
+        }
+    }
+}
