98e64f06d83d53bd473f376a29c77f4f871a93a4
Viktor Jakovlev <vicebad@hotmail.com>
2021-05-11 19:09:44 +0200

    class 07 init

--- G5/class06 - AnonymousMethodsAndAdvancedLINQ/code/Class06/AnonymousMethods/Program.cs
+++ G5/class06 - AnonymousMethodsAndAdvancedLINQ/code/Class06/AnonymousMethods/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AnonymousMethods
 {
@@ -9,6 +10,7 @@ namespace AnonymousMethods
         {
             List<string> names = new List<string>() { "Bob", "Jill", "Wayne", "Greg", "John", "Anne" };
 
+            List<string> newList = names.Where(name => name == "John").ToList();
 
             Func<bool> isNamesEmpty = () => names.Count == 0;
             Func<bool> isNamesEmpty2 = () => names.Count > 0;
@@ -29,6 +31,31 @@ namespace AnonymousMethods
             bool result4 = CheckNumbers(10, 5, (x, y) => x == y);
             Console.WriteLine(result3);
             Console.WriteLine(result4);
+
+
+            // FUNC
+            // Func always has a return value 
+
+            // Example of function with no input parameters
+            Func<int> robertFunction = () => 3 + 2 + 5;
+
+            // Example of function with 1 input parameter
+            Func<List<string>, bool> robertFunction2 = names => 
+            {
+                return names.Count == 0;
+            };
+
+            Func<List<string>, bool> robertFunction3 = names => names.Count == 0;
+
+            // Example of function with 2 input parameters
+            Func<string, string, string> careateFullName = (name, lastName) => $"{name} {lastName}";
+
+            var fullName = careateFullName("Viktor", "Jakovlev");
+            var fullName2 = careateFullName("Angela", "Kostadinova");
+
+            // ACTION
+            // Action is always void
+
         }
 
         public static bool CheckNumbers(int number1, int number2, Func<int, int, bool> func) 
