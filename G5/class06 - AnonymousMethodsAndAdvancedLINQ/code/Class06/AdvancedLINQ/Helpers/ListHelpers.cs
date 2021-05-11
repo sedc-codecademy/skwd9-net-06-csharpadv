be8d6362a278d3dfb47a0a9db42098bc9e5e1eed
Viktor Jakovlev <vicebad@hotmail.com>
2021-05-11 20:20:53 +0200

    Advanced LINQ data set

---
+++ G5/class06 - AnonymousMethodsAndAdvancedLINQ/code/Class06/AdvancedLINQ/Helpers/ListHelpers.cs
@@ -0,0 +1,32 @@
+﻿using AdvancedLINQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedLINQ.Helpers
+{
+	public static class ListHelper
+	{
+		public static void PrintSimple<T>(this List<T> list)
+		{
+			Console.WriteLine("Printing...");
+			Console.WriteLine("------------------------------");
+			foreach (T item in list)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("------------------------------");
+		}
+
+		public static void PrintEntities<T>(this List<T> list) where T : BaseEntity
+		{
+			Console.WriteLine($"Printing {list[0].GetType().Name}s...");
+			Console.WriteLine("------------------------------");
+			foreach (T item in list)
+			{
+				Console.WriteLine(item.Info());
+			}
+			Console.WriteLine("------------------------------");
+		}
+	}
+}
