38d348eaf44df4cd76211ccab0c395489dad9397
Martin <panovski.martin93@gmail.com>
2021-05-11 23:04:00 +0200

    Class 07 application initial structure added

---
+++ G6/Class07/SEDC.TryBeingFit/SEDC.TryBeingFit.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEDC.TryBeingFit.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ G6/Class07/SEDC.TryBeingFit/SEDC.TryBeingFit.Domain/Core/Enums/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.TryBeingFit.Domain.Core.Enums
+{
+    public enum UserRole
+    {
+        Standard = 1,
+        Premium = 2,
+        Trainer = 3
+    }
+}
