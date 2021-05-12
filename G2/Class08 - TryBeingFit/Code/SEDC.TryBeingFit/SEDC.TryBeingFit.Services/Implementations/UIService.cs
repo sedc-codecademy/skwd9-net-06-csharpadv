1a84b10385e23d020cce2c9d502b85efed0f5b53
tstojanovska <stojanovskatanja03@gmail.com>
2021-05-12 21:06:35 +0200

    Class 08

---
+++ G2/Class08 - TryBeingFit/Code/SEDC.TryBeingFit/SEDC.TryBeingFit.Services/Implementations/UIService.cs
@@ -0,0 +1,48 @@
+﻿using SEDC.TryBeingFit.Domain.Enums;
+using SEDC.TryBeingFit.Services.Helpers;
+using SEDC.TryBeingFit.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.TryBeingFit.Services.Implementations
+{
+    public class UIService : IUIService
+    {
+        public int ChooseMenuItem(List<string> menuItems)
+        {
+            while (true)
+            {
+                
+                for (int i = 0; i < menuItems.Count; i++)
+                {
+                    Console.WriteLine($"[{i + 1}] {menuItems[i]}");
+                }
+                string input = Console.ReadLine();
+                int choice = ValidationHelper.ValidateNumber(input, menuItems.Count);
+                if (choice == -1)
+                {
+                    MessageHelper.PrintMessage("You must enter a valid option", ConsoleColor.Red);
+                   
+                    continue;
+                }
+                return choice;
+            }
+
+        }
+
+        public int LogInMenu()
+        {
+            List<string> menuItems = new List<string> { "LogIn", "Register" };
+            Console.WriteLine("Choose option");
+            return ChooseMenuItem(menuItems);
+        }
+
+        public int RoleMenu()
+        {
+            List<string> menuItems = Enum.GetNames(typeof(UserRole)).ToList(); //gets the names of members of UserRole enum
+            Console.WriteLine("Choose role");
+            return ChooseMenuItem(menuItems);
+        }
+    }
+}
---
+++ G2/Class08 - TryBeingFit/Code/SEDC.TryBeingFit/SEDC.TryBeingFit.Services/Interfaces/IUIService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SEDC.TryBeingFit.Services.Interfaces
+{
+    public interface IUIService
+    {
+        int ChooseMenuItem(List<string> menuItems);
+        int LogInMenu();
+        int RoleMenu();
+    }
+}
