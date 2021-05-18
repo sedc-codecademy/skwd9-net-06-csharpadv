a90e3a536d1534453ae4209b7ccf94a174d68606
Viktor Jakovlev <vicebad@hotmail.com>
2021-05-18 21:14:43 +0200

    delegates class init

---
+++ G5/class07 - DelegatesAndEvents/code/Class07/Delegates_PlasticExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Delegates_PlasticExample
+{
+    class Program
+    {
+        public delegate void GreetingsFunctionDelegate(string message);
+
+        static void Main(string[] args)
+        {
+            // Delegate is a type safe function pointer. Delegates holds a reference (pointer) to a function.
+            // The signature of the delegate must match the signature of the function that delegates points to.
+
+            GreetingsFunctionDelegate delegate1 = new GreetingsFunctionDelegate(Greetings);
+            GreetingsFunctionDelegate delegate2 = new GreetingsFunctionDelegate(Bye);
+
+            GreetingsFunctionDelegate delegate3 = new GreetingsFunctionDelegate(name => Console.WriteLine($"Hello {name} from the anonymous function."));
+
+            delegate1("Viktor");
+            delegate2("Angela");
+            delegate3("Greg");
+
+            Console.ReadLine();
+        }
+
+        public static void Greetings(string name) 
+        {
+            Console.WriteLine($"Hello {name}.");
+        }
+
+        public static void Bye(string name)
+        {
+            Console.WriteLine($"Bye {name}.");
+        }
+    }
+}
