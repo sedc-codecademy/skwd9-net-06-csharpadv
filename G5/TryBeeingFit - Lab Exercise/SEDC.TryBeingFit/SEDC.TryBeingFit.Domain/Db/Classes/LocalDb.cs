2d98a4e01513a8ac662357d933d966807e603c13
Viktor Jakovlev <vicebad@hotmail.com>
2021-05-18 19:31:17 +0200

    trybeeingfit generic db finished

--- G5/TryBeeingFit - Lab Exercise/SEDC.TryBeingFit/SEDC.TryBeingFit.Domain/Db/Classes/LocalDb.cs
+++ G5/TryBeeingFit - Lab Exercise/SEDC.TryBeingFit/SEDC.TryBeingFit.Domain/Db/Classes/LocalDb.cs
@@ -2,42 +2,50 @@
 using SEDC.TryBeingFit.Domain.Db.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SEDC.TryBeingFit.Domain.Db.Classes
 {
     public class LocalDb<T> : IDb<T> where T : BaseEntity
     {
+        public int IdCounter { get; set; }
         private List<T> Db;
 
         public LocalDb()
         {
             Db = new List<T>();
+            IdCounter = 1;
         }
 
         public List<T> GetAll()
         {
-            throw new NotImplementedException();
+            return Db;
         }
 
         public T GetById(int id)
         {
-            throw new NotImplementedException();
+            return Db.SingleOrDefault(x => x.Id == id);
         }
 
         public int Insert(T entity)
         {
-            throw new NotImplementedException();
+            entity.Id = IdCounter;
+            Db.Add(entity);
+            IdCounter++;
+            return entity.Id;
         }
 
         public void RemoveById(int id)
         {
-            throw new NotImplementedException();
+            T item = GetById(id);
+            if (item != null) Db.Remove(item); 
         }
 
         public void Update(T entity)
         {
-            throw new NotImplementedException();
+            T item = GetById(entity.Id);
+            item = entity;
         }
     }
 }
