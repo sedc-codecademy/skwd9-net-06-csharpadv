1afdc14fad9df9f0c0e69f7e0316688e3446ed82
Viktor Jakovlev <vicebad@hotmail.com>
2021-05-18 18:56:40 +0200

    trybeeingfit generic db created

---
+++ G5/TryBeeingFit - Lab Exercise/SEDC.TryBeingFit/SEDC.TryBeingFit.Domain/Db/Classes/LocalDb.cs
@@ -0,0 +1,43 @@
+﻿using SEDC.TryBeingFit.Domain.Core.Entities;
+using SEDC.TryBeingFit.Domain.Db.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.TryBeingFit.Domain.Db.Classes
+{
+    public class LocalDb<T> : IDb<T> where T : BaseEntity
+    {
+        private List<T> Db;
+
+        public LocalDb()
+        {
+            Db = new List<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
