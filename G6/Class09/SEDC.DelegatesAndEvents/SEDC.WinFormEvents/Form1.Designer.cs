9f9e7b60d599a1387dd3097340a5cdc3a9fd7f02
Martin <panovski.martin93@gmail.com>
2021-05-18 21:15:41 +0200

    Class 09 demo code added

---
+++ G6/Class09/SEDC.DelegatesAndEvents/SEDC.WinFormEvents/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace SEDC.WinFormEvents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.helloTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 101);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Say goodbye!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // helloTxt
+            // 
+            this.helloTxt.AutoSize = true;
+            this.helloTxt.Location = new System.Drawing.Point(384, 163);
+            this.helloTxt.Name = "helloTxt";
+            this.helloTxt.Size = new System.Drawing.Size(0, 13);
+            this.helloTxt.TabIndex = 1;
+            this.helloTxt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.helloTxt);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label helloTxt;
+    }
+}
+
