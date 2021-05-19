da97027dd471349b54fbeb5c582794cdf68f9e5a
tstojanovska <stojanovskatanja03@gmail.com>
2021-05-19 20:49:15 +0200

    Class 10

---
+++ G2/Class10 - Disposing, Nullable, Named and Optional/Code/Disposing/Disposing/InputOutputHelpers/OurReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Disposing.InputOutputHelpers
+{
+    public class OurReader : IDisposable
+    {
+        private StreamReader _streamReader;
+        private bool _disposedValue;
+
+        public OurReader(string path)
+        {
+            _streamReader = new StreamReader(path);
+            _disposedValue = false;
+        }
+
+        public string Read()
+        {
+            return _streamReader.ReadToEnd();
+        }
+        public void Dispose()
+        {
+            if (!_disposedValue)
+            {
+                _streamReader?.Dispose();
+                _disposedValue = true;
+            }
+        }
+    }
+}
