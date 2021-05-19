7ae745b7228244f2db5e9c23291f286865914904
Manass2 <panemanass2@gmail.com>
2021-05-19 21:59:31 +0200

    bonus

---
+++ G4/Class10/bonus material/StringBuilderDemoCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderDemoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String word = "hi";
+            StringBuilder newString = new StringBuilder("Hello");
+
+            newString.Append(" G4");
+            newString.Append('!', 3);
+            newString.AppendLine();
+            newString.Append("We are learning StringBuilder today!");
+            //newString.Remove(0, 6);
+            //newString.Replace("We are learning StringBuilder today!", "Lets go home");
+            newString.Insert(0, "Testing... Testing...   ");
+
+            Console.WriteLine(newString);
+
+            Console.ReadLine();
+        }
+    }
+}
