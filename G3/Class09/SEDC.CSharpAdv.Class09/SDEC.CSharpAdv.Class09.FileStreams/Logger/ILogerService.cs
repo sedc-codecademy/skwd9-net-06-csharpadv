f6fd93ec96eda07e1cffa43f1e55ac9ab3aa2a36
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-05-20 21:09:12 +0200

    Class09 part-2

---
+++ G3/Class09/SEDC.CSharpAdv.Class09/SDEC.CSharpAdv.Class09.FileStreams/Logger/ILogerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDEC.CSharpAdv.Class09.FileStreams.Logger
+{
+    public interface ILogerService
+    {
+        void LogUsage(string username, bool isLogedIn);
+        void LogError(string error, string message, string username);
+    }
+}
