a69cbebe38b013bf0c9ca6186959ce6eb04e62ca
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-05-25 20:45:45 +0200

    Class10 part-final

---
+++ G3/Class10/SEDC.CSharpAdv.Class10/SEDC.CSharpAdv.Class10.Nullable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SEDC.CSharpAdv.Class10.Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            Console.WriteLine(person.Id); // int by default is 0
+            Console.WriteLine(person.Score); // null by default
+
+            // person.Id = null; // we can't put null in an int
+            person.Score = null; // if int is declared nullable we can set it to null
+
+            Console.WriteLine(person.IsEmployed); // bool default is false
+            Console.WriteLine(person.HasPet); // null by default
+
+            // person.IsEmployed = null; // bool default is false and cannot be set to null
+            person.HasPet = null; // if bool is declared nullable can be set to null
+
+            Console.WriteLine(person.Name);
+
+            person.Name = null; // string is nullable by default
+            person.Name = "Trajan";
+
+            person = null;
+
+            Console.ReadLine();
+        }
+
+        public class Person
+        {
+            public int Id { get; set; }
+            public Nullable<int> Score { get; set; } // old way
+            public bool IsEmployed { get; set; }
+            public bool? HasPet { get; set; } // new way
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ G3/Class10/SEDC.CSharpAdv.Class10/SEDC.CSharpAdv.Class10.OptionalNamedParameters/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SEDC.CSharpAdv.Class10.OptionalNamedParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NoOptional(2, 3, "+");
+            // NoOptional(2, 3); // here third param is not optional
+
+            SomeOptional(2, 3);
+            SomeOptional(10, 7, "-");
+
+            AllOptional();
+            AllOptional(1);
+            AllOptional(2, 3);
+            AllOptional(5, 7, "-");
+
+            NullParam(3);
+            NullParam(null);
+
+            NoOptional(num1: 3, operation: "-", num2: 2);
+            NoOptional(10, operation: "+", num2: 7);
+
+            // NoOptional(operation: "+", 10, 7); // we can only do random order if ALL arguments are named
+
+            Console.ReadLine();
+        }
+
+        static void CheckOperator(int num1, int num2, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"{num1} {operation} {num2} = {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{num1} {operation} {num2} = {num1 - num2}");
+                    break;
+                default:
+                    Console.WriteLine($"The app does not work with {operation}");
+                    break;
+            }
+        }
+
+        static void NoOptional(int num1, int num2, string operation)
+        {
+            CheckOperator(num1, num2, operation);
+        }
+
+        static void SomeOptional(int num1, int num2, string operation = "+")
+        {
+            CheckOperator(num1, num2, operation);
+        }
+        
+        static void AllOptional(int num1 = 5, int num2 = 3, string operation = "+")
+        {
+            CheckOperator(num1, num2, operation);
+        }
+
+        static void NullParam(int? num1)
+        {
+            string a = num1 != null ? num1.ToString() : "Null";
+            Console.WriteLine(a);
+        }
+    }
+}
