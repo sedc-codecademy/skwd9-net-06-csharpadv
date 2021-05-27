0e6d28a27ded9d552d2000915a9db65875eec784
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-05-27 20:58:03 +0200

    Class11 part-final

---
+++ G3/Class11/SEDC.CShaprAdv.Class11/SEDC.CSharpAdv.Class11.SystemDatabase/Program.cs
@@ -0,0 +1,59 @@
+﻿using SEDC.CSharpAdv.Class11.SystemDatabase.Entites;
+using System;
+using System.Collections.Generic;
+
+namespace SEDC.CSharpAdv.Class11.SystemDatabase
+{
+    class Program
+    {
+        static Db<Student> _studentDb = new Db<Student>();
+        static Db<Subject> _subjectDb = new Db<Subject>();
+
+        public static void Seed()
+        {
+            if(_studentDb.GetAll().Count == 0)
+            {
+                Console.WriteLine("===========INSERTING STUDENTS=============");
+                _studentDb.Insert(new Student("Bob", "Bobsky", 25));
+                _studentDb.Insert(new Student("Trajan", "Stevkovski", 26));
+                _studentDb.Insert(new Student("Nikola", "Dalchevski", 23));
+            }
+
+            if (_subjectDb.GetAll().Count == 0)
+            {
+                Console.WriteLine("===========INSERTING SUBJECTS=============");
+                _subjectDb.Insert(new Subject("C# Advanced", Academy.Code, 60));
+                _subjectDb.Insert(new Subject("Introduction to Servers", Academy.Network, 40));
+                _subjectDb.Insert(new Subject("Photoshop", Academy.Design, 32));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Seed();
+
+            List<Student> students = _studentDb.GetAll();
+            List<Subject> subjects = _subjectDb.GetAll();
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.Info());
+            }
+
+            foreach (var subject in subjects)
+            {
+                Console.WriteLine(subject.Info());
+            }
+
+            Student chuck = new Student("Chuck", "Noris", 22);
+            int id = _studentDb.Insert(chuck);
+            Console.WriteLine(id);
+
+            Student chuckById = _studentDb.GetById(id);
+            Console.WriteLine(chuckById.Info());
+
+
+            Console.ReadLine();
+        }
+    }
+}
