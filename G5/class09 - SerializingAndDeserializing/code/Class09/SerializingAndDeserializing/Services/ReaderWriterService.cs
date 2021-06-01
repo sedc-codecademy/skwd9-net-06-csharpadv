6b8b99108fe6f11866f3de1c8525cc078b60fcc6
Viktor Jakovlev <vicebad@hotmail.com>
2021-06-01 19:08:24 +0200

    updates

---
+++ G5/class09 - SerializingAndDeserializing/code/Class09/SerializingAndDeserializing/Services/ReaderWriterService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SerializingAndDeserializing.Services
+{
+    public class ReaderWriterService
+    {
+        public string ReadFile(string path) 
+        {
+            string result = "";
+
+            if (!File.Exists(path)) 
+            {
+                return "File does not exist!";
+            }
+
+            using (StreamReader sr = new StreamReader(path, true)) 
+            {
+                result = sr.ReadToEnd();
+            }
+
+            Console.WriteLine("Succesfully read a file!");
+            return result;
+        }
+
+        public void WriteFile(string path, string data) 
+        {
+            using (StreamWriter sw = new StreamWriter(path)) 
+            {
+                sw.WriteLine(data);
+            }
+
+            Console.WriteLine("Succesfully written in a file!");
+        }
+    }
+}
