45043bb5b87613c2eb0708f4e05ff5f890048ab1
tstojanovska <stojanovskatanja03@gmail.com>
2021-06-02 21:01:15 +0200

    Class 14

---
+++ G2/Class14 - Good practices/Code/ExceptionsExample/ExceptionsExample/CustomExceptions/NumberException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionsExample.CustomExceptions
+{
+    public class NumberException : Exception
+    {
+        public NumberException(string message) : base (message)
+        {
+
+        }
+    }
+}
