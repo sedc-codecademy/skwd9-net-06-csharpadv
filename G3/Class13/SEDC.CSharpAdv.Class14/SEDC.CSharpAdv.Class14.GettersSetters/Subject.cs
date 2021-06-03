711d7aef3f3b7cc170112efddea827e42e629b2a
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-06-03 18:33:02 +0200

    Class14 part-1

---
+++ G3/Class13/SEDC.CSharpAdv.Class14/SEDC.CSharpAdv.Class14.GettersSetters/Subject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.CSharpAdv.Class14.GettersSetters
+{
+    public class Subject : BaseEntity
+    {
+        //public override int Name { get; set; }
+        public override string Name { get { return _name.ToUpper(); } set { _name = value; } }
+
+    }
+}
---
+++ G3/Class13/SEDC.CSharpAdv.Class14/SEDC.CSharpAdv.Class14.GettersSetters/UsingLinq.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SEDC.CSharpAdv.Class14.GettersSetters
+{
+    static class UsingLinq
+    {
+        //public IEnumerable<T> Where<T>(IEnumerable<T> list, Func<T, bool> predicate)
+        //{
+        //    var filteredList = new List<T>();
+        //    foreach (var item in list)
+        //    {
+        //        if (predicate(item))
+        //        {
+        //            filteredList.Add(item);
+        //        }
+        //    }
+        //    return filteredList;
+        //}
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> list, Func<T, bool> predicate)
+        {
+            var filteredList = new List<T>();
+            foreach (var item in list)
+            {
+                if (predicate(item))
+                {
+                    filteredList.Add(item);
+                }
+            }
+            return filteredList;
+        }
+    }
+}
