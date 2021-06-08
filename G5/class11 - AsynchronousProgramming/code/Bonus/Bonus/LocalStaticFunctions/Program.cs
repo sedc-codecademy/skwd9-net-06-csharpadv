786f8bd27249a9695e568dd2de57932c91d95acc
Viktor Jakovlev <vicebad@hotmail.com>
2021-06-08 21:11:38 +0200

    bonus added

---
+++ G5/class11 - AsynchronousProgramming/code/Bonus/Bonus/LocalStaticFunctions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LocalStaticFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result1 = Caclculate('+', 5, 10);
+            var result2 = Caclculate('-', 15, 12);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+            Console.ReadLine();
+        }
+
+        public static int Caclculate(char operation, int num1, int num2)
+        {
+            int LocalAdd(int a, int b)
+            {
+                return a + b;
+            };
+
+            int LocalSubtract(int a, int b)
+            {
+                return a - b;
+            };
+
+            switch (operation)
+            {
+                case '+':
+                    return LocalAdd(num1, num2);
+                case '-':
+                    return LocalSubtract(num1, num2);
+                default:
+                    return 0;
+            }
+        }
+    }
+}
